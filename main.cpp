2238ce310cfdaa1be7a4614340be68d7bdfca559
AERTY  PINTO DOS SANTOS <57457645+Aertyps@users.noreply.github.com>
2021-06-26 19:25:43 +0000

    desenrolando a leitura

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include"arquivo.cpp"
 #include<math.h>
+#include <fstream>
 
 using namespace std;
 
 struct no{
   no* proximo;
-  int lado1;
-  int lado2;
-  int lado3;
+  float lado1;
+  float lado2;
+  float lado3;
 };
 
- no* cria(int ld1, int ld2, int ld3){
+ no* cria(float ld1, float ld2, float ld3){
     no* n = new no;
     n->lado1 = ld1;
     n->lado2 = ld2;
@@ -45,7 +45,7 @@ bool finalLista(lista* plista){
   return (plista->marcador == NULL);
 }
 
-void incluirFim(lista* plista, int ld1, int ld2, int ld3){
+void incluirFim(lista* plista, float ld1, float ld2, float ld3){
   no* n;
   n = cria(ld1, ld2, ld3);
 
@@ -65,20 +65,53 @@ void imprimirLista(lista* pLista){
 
   pLista->marcador = pLista->primeiro;
   cout<<"Triangulos inseridos\n";
+
   while(!finalLista(pLista)){
     cout<<"Lado1: "<<pLista->marcador->lado1<<" Lado2: "<<pLista->marcador->lado2<<" Lado3: "<<pLista->marcador->lado3<<"\n";
     pLista->marcador = pLista->marcador->proximo;
   }
 }
 
+string correcaoF(string f){
+
+  bool ponto = false, libera = false;
+  string nova ="";
+  char  ant =' ';
+
+  for(int i = (f.length () - 1); i >= 0 ; i--){
+
+      if((f[i] !='.')&&(f[i] != '0')){
+        if(f[i-1] == '.'){
+          libera = true;
+        }
+      }
+      
+      if(((f[i] != '0') && ponto)|| libera){
+        nova = f[i] + nova;
+      }
+
+      if(f[i] == '.'){
+        ponto = true;
+        libera = true;
+      }
+
+  }
+
+  return nova;
+}
+
 string preEscrita(lista* pLista){
 
   pLista->marcador = pLista->primeiro;
-  string txt = "",num_str ="";
+  string txt = "", l1, l2, l3;
  
   while(!finalLista(pLista)){
 
-    txt +="l1:"+ to_string(pLista->marcador->lado1)+"l2:"+to_string(pLista->marcador->lado2)+"l3:"+to_string(pLista->marcador->lado3);
+    l1 = correcaoF(to_string(pLista->marcador->lado1));
+    l2 = correcaoF(to_string(pLista->marcador->lado2));
+    l3 = correcaoF(to_string(pLista->marcador->lado3));
+
+    txt +="l1:"+l1+"l2:"+l2+"l3:"+l3;
 
     pLista->marcador = pLista->marcador->proximo;
 
@@ -116,10 +149,67 @@ void excluirPosicao(lista* pLista, int pos){
  
 }
 
+void leitura(lista* pLista){
+
+    string texto = "",txt ="", l1, l2, l3;
+    bool a = false, b = false, c = false;
+    ifstream leitor;
+    leitor.open("banco.txt", ios::in);
+
+    while(!leitor.eof()){
+      leitor>>txt;
+
+      for(int i = 0; i < txt.length(); i++){
+
+        if(txt[i] == 'l'){
+            a = false;
+            b = false;
+            c = false;
+        }
+
+        if(a){
+          l1+= txt[i];
+        }else if(b){
+          l2+= txt[i];
+        }else if(c){
+          l3+= txt[i];
+        }
+
+        if(txt[i] == ':'){
+           
+            if(txt[i-1] == '1'){
+              a = true;
+            }else if(txt[i-1] == '2'){
+              b = true;
+            }else if(txt[i-1] == '3'){
+              c = true;
+            }
+        }
+
+      }
+      cout<<"\n "<<l1<<" "<<l2<<" "<<l3;
+      float num_float = std::stof(str);
+      l1 = "";
+      l2 = "";
+      l3 = "";
+    }
+
+    leitor.close();
+    //return texto;
+  }
+
+  bool escrita(string txt){
+    ofstream escritor;
+    escritor.open("banco.txt", ios::out);
+    escritor<<txt;
+    escritor.close();
+    return true;
+  }
+
 void gravar(lista* pLista){
 
   int qtd = 0;
-  int l1=0, l2=0, l3=0;
+  float l1=0, l2=0, l3=0;
   cout<<"\nQuantos elementos deseja gravar:\n";
   cin>>qtd;
 
@@ -139,20 +229,18 @@ void gravar(lista* pLista){
     qtd--;
   }
 
-  Arquivo arquivo;
-  arquivo.Escrita(preEscrita(pLista));
+  
+  escrita(preEscrita(pLista));
   
 }
 
 void ler(lista* pLista){
+  leitura(pLista);
   imprimirLista(pLista);
 }
 
 int main() {
-
-  lista* tLista = new lista;
-  iniciarLista(tLista);
-
+  lista* tLista;
   int opcao = 0;
  
   do{
@@ -167,12 +255,19 @@ int main() {
 
       case 0:
           cout<<"Saindo do programa";
+
         break;
       case 1:
+          tLista = new lista;
+          iniciarLista(tLista);
           gravar(tLista);
+
         break;
       case 2:
+          tLista = new lista;
+          iniciarLista(tLista);
           ler(tLista);
+          
         break;
       default:
         cout<<"Opcao nao programada";
